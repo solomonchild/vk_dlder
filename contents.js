abc3e1414108631b3b324664936341c6dd79f445
Vlad <solomonchild@gmail.com>
2014-08-17 22:59:47 +0300

    Added scrolling handler, show/hide comments handler

--- contents.js
+++ contents.js
@@ -19,36 +19,65 @@ function onDlClick(event) {
     console.log(event.toElement.getAttribute('url'));
     return true;
 }
+var dHeight;
 
+function yo() {
+
+}
 function getAudioElemList() {
+
+    show_comments = document.getElementsByClassName('wr_header');
+    Array.prototype.forEach.call(show_comments, function(show_comment) {
+            show_comment.onclick = function() {
+                setTimeout(function() {
+                    console.log("Hi");
+                    getAudioElemList();
+                }, 500);
+            }
+    });
+
+
     play_buttons = document.getElementsByClassName('play_btn_wrap');
     Array.prototype.forEach.call(play_buttons, function(play_btn) {
-            url = getAudioUrl(play_btn);
-            dl_a = document.createElement('a');
-            dl_a.setAttribute('href', url);
-            dl_a.setAttribute('download', "");
-            dl_a.setAttribute("style", "padding: 8px; padding-top:0px;");
-            dl_a.onclick = onDlClick;
-
-            dl_div = document.createElement('div');
-            iconUrl = chrome.extension.getURL("images/play.gif"); 
-            dl_div.setAttribute("style", "background:url('" + iconUrl + "') no-repeat 0px 0px !important;");
-            dl_div.className = 'play_new';
-            dl_div.appendChild(dl_a);
-
-
-
             tr = play_btn.parentNode.parentNode;
             before_ins = tr.childNodes[3]; //third one is "info" td, insert before it
+            if(before_ins.className == "info") {
+                url = getAudioUrl(play_btn);
+                dl_a = document.createElement('a');
+                dl_a.setAttribute('href', url);
+                dl_a.setAttribute('download', "");
+                dl_a.setAttribute("style", "padding: 8px; padding-top:0px;");
+                dl_a.onclick = onDlClick;
 
-            fresh_td = document.createElement('td');
-            fresh_td.setAttribute('style', 'padding-top:6px; padding-right:6px; ');
-            fresh_td.appendChild(dl_div);
-            tr.insertBefore(fresh_td, before_ins);
+                dl_div = document.createElement('div');
+                iconUrl = chrome.extension.getURL("images/play.gif"); 
+                dl_div.setAttribute("style", "background:url('" + iconUrl + "') no-repeat 0px 0px !important;");
+                dl_div.className = 'play_new';
+                dl_div.appendChild(dl_a);
+
+
+                fresh_td = document.createElement('td');
+                fresh_td.setAttribute('style', 'padding-top:6px; padding-right:6px; ');
+                fresh_td.appendChild(dl_div);
+                tr.insertBefore(fresh_td, before_ins);
+            }
     });
 }
 
 function getVideoElem() {
     return document.getElementsByName('video_player');
 }
-getAudioElemList() ;
+
+function onScroll(event) {
+    if(window.pageYOffset > dHeight) {
+        dHeight = document.body.offsetHeight;
+        getAudioElemList();
+    }
+}
+
+function main() {
+    dHeight = document.body.offsetHeight;
+    window.onscroll = onScroll;
+    getAudioElemList() ;
+};
+main();
